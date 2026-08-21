8db6d399ce7d5f92cf257d38b6860dd87473910f
Umut Ertuğrul Daşgın <105071264+ertugruldasgin@users.noreply.github.com>
2026-08-21 18:51:04 +0300

    nvim: treesitter

---
+++ home/dot_config/nvim/lua/car6on/lazy/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter").setup()
+      require("nvim-treesitter").install({
+        "c", "cpp", "go", "gomod", "gosum", "gotmpl",
+        "lua", "python", "bash", "zig",
+        "typescript", "tsx", "javascript", "html", "css",
+        "json", "yaml", "toml", "dockerfile",
+        "sql", "make", "markdown", "markdown_inline",
+      })
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function() pcall(vim.treesitter.start) end,
+      })
+    end,
+  },
+}
+
