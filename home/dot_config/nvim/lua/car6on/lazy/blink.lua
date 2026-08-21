c30613f8253f5ee5311ba8d8c617aa75e0bd7970
Umut Ertuğrul Daşgın <105071264+ertugruldasgin@users.noreply.github.com>
2026-08-21 23:54:15 +0300

    nvim: edit blink and add autopairs

--- home/dot_config/nvim/lua/car6on/lazy/blink.lua
+++ home/dot_config/nvim/lua/car6on/lazy/blink.lua
@@ -3,9 +3,18 @@ return {
 		"saghen/blink.cmp",
 		version = "*",
 		opts = {
-			keymap = { preset = "default" },
+			keymap = {
+				preset = "super-tab",
+			},
+			appearance = {
+				nerd_font_variant = "mono",
+			},
 			completion = {
-				documentation = { auto_show = true },
+				documentation = { auto_show = true, auto_show_delay_ms = 200 },
+				ghost_text = { enabled = true },
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
 			},
 			signature = { enabled = true },
 		},
